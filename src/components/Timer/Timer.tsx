fa5c7072ae937f64acda7ecc122544d2c67610d4
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-02-29 11:40:06 +0300

    feat: set timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,7 +1,31 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
+import styles from './Timer.module.scss';
 
 const Timer: FC = () => {
-  return <div>Timer</div>
+  const [minutes, setMinutes] = useState(60);
+  const [seconds, setSeconds] = useState(60);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(interval);
+        } else {
+          setMinutes(prevState => prevState - 1);
+          setSeconds(60);
+        }
+      } else {
+        setSeconds(prevState => prevState - 1);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [minutes, seconds]);
+
+  return (
+    <div className={styles.timer}>
+      <h1>{`${minutes.toString()}:${seconds.toString()}`}</h1>
+    </div>
+  )
 };
 
 export default Timer;
